723411417fa6e8b59edc34084941bd0f2e28117d
fallacious franky <frank@lump.lumpnet>
2015-04-19 11:13:54 +0200

    modified VMD visualisation files in test directory

--- tests/surface/plot_2d_surf.py
+++ tests/surface/plot_2d_surf.py
@@ -8,7 +8,7 @@ from ase.units import Bohr
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-dat = np.loadtxt('0_2dsurfdown.dat')*Bohr
+dat = np.loadtxt('out_0_2dsurfdown.dat')*Bohr
 
 x = dat[0,1:].copy()
 y = dat[1:,0].copy()
@@ -18,7 +18,7 @@ X, Y = np.meshgrid(x,y)
 #ax.plot_surface(X, Y, dat[1:,1:])
 ax.plot_wireframe(X, Y, dat[1:,1:])
 
-dat = np.loadtxt('0_2dsurfup.dat')*Bohr
+dat = np.loadtxt('out_0_2dsurfup.dat')*Bohr
 
 x = dat[0,1:].copy()
 y = dat[1:,0].copy()
