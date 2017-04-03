dcedcee47a6023060ef9a7cf9ea875af3459d268
Frank Uhlig <uhlig.frank@gmail.com>
2017-04-03 12:12:37 +0200

    a bit of cleanup

--- src/surf.c
+++ src/surf.c
@@ -746,57 +746,6 @@ double myconstraint(unsigned n, const double *x, double *grad, void *data)
     return density - d->surfcut;
  }
 
-// double myfunc(unsigned n, const double *x, double *grad, void *my_func_data)
-// {
-//     if (grad) {
-//         grad[0] = 0.0;
-//         grad[1] = 0.5 / sqrt(x[1]);
-//     }
-//     return sqrt(x[1]);
-// }
-// 
-// typedef struct {
-//     double a, b;
-// } my_constraint_data;
-// 
-// double myconstraint(unsigned n, const double *x, double *grad, void *data)
-// {
-//     my_constraint_data *d = (my_constraint_data *) data;
-//     double a = d->a, b = d->b;
-//     if (grad) {
-//         grad[0] = 3 * a * (a*x[0] + b) * (a*x[0] + b);
-//         grad[1] = -1.0;
-//     }
-//     return ((a*x[0] + b) * (a*x[0] + b) * (a*x[0] + b) - x[1]);
-//  }
-// 
-// double lb[2] = { -HUGE_VAL, 0 }; /* lower bounds */
-// nlopt_opt opt;
-// 
-// opt = nlopt_create(NLOPT_LD_MMA, 2); /* algorithm and dimensionality */
-// nlopt_set_lower_bounds(opt, lb);
-// nlopt_set_min_objective(opt, myfunc, NULL);
-// 
-// my_constraint_data data[2] = { {2,0}, {-1,1} };
-// 
-// nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-8);
-// nlopt_add_inequality_constraint(opt, myconstraint, &data[1], 1e-8);
-// 
-// 
-// nlopt_set_xtol_rel(opt, 1e-4);
-// 
-// double x[2] = { 1.234, 5.678 };  /* some initial guess */
-// double minf; /* the minimum objective value, upon return */
-// 
-// if (nlopt_optimize(opt, x, &minf) < 0) {
-//     printf("nlopt failed!\n");
-// }
-// else {
-//     printf("found minimum at f(%g,%g) = %0.10g\n", x[0], x[1], minf);
-// }
-// 
-// nlopt_destroy(opt);
-
 #ifdef HAVE_NLOPT
 real get_opt_distance_to_surface( real *init_guess, real *mepos, int *mask, atom_t * atoms, real *zeta, real surfcut, real *pbc, real resolution, int periodic, real *bnds )
 {
@@ -841,30 +790,12 @@ real get_opt_distance_to_surface( real *init_guess, real *mepos, int *mask, atom
 
   nlopt_add_equality_constraint(opt, myconstraint, &cons_data, 1e-6);
 
-  // my_constraint_data_type cons_data[DIM];
-
-  // int d;
-  // for ( d=0; d<DIM; d++ ) {
-  //   cons_data[d].mask = mask;
-  //   cons_data[d].atoms = atoms;
-  //   cons_data[d].zeta = zeta;
-  //   cons_data[d].surfcut = surfcut;
-  //   cons_data[d].pbc = pbc;
-  //   cons_data[d].resolution = resolution;
-  //   cons_data[d].periodic = periodic;
-  // }
-  
-  // nlopt_add_equality_constraint(opt, myconstraint, &cons_data[0], 1e-8);
-  // nlopt_add_equality_constraint(opt, myconstraint, &cons_data[1], 1e-8);
-  // nlopt_add_equality_constraint(opt, myconstraint, &cons_data[2], 1e-8);
-  
   nlopt_set_xtol_rel(opt, 1e-4);
   // nlopt_set_maxtime(opt, 1.);
   
   double x[3] = { init_guess[0], init_guess[1], init_guess[2] };  /* some initial guess */
   double minf; /* the minimum objective value, upon return */
   
-  // printf("STARTING OPTIMIZATION %g,%g,%g\n", init_guess[0], init_guess[1], init_guess[2]);
   if (nlopt_optimize(opt, x, &minf) < 0) {
       printf("nlopt failed!\n");
   }
